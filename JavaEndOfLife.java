74a05c416cb183f19a02397c6ad784fa0720c232
Lavanya D <139872480+lavs4@users.noreply.github.com>
2025-05-23 12:50:04 +0530

    Added JavaEndOfLife.java

---
+++ JavaEndOfLife.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaEndOfLife {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int lineNumber = 1;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(lineNumber + " " + line);
+            lineNumber++;
+        }
+
+        scanner.close();
+    
+    }
+}
